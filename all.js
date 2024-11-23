b9e4f7939e7d3f4548dcd94533c81316fa8bb775
ajaymakvana05 <amakavana460@gmail.com>
2024-11-23 05:59:27 +0530

    complete landing page

---
+++ all.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            backgroundImage: {
+                'hero-pattern': "url('/img/hero-pattern.svg')",
+      }
+              }
+        },
+    };
+
+
+
+
+ 
